4e6bb69893eee9057ae340ffc42a48149d367fb9
Johnathan Vastola <johnnyvastola@gmail.com>
2025-11-30 17:56:05 -0800

    jni

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -8,3 +8,19 @@ struct LiveKitExtension;
 
 #[gdextension]
 unsafe impl ExtensionLibrary for LiveKitExtension {}
+
+#[cfg(target_os = "android")]
+#[no_mangle]
+pub unsafe extern "system" fn JNI_OnLoad(vm: *mut jni::sys::JavaVM, _: *mut std::ffi::c_void) -> jni::sys::jint {
+    android_logger::init_once(
+        android_logger::Config::default().with_max_level(log::LevelFilter::Trace),
+    );
+    log::info!("JNI_OnLoad called");
+    
+    let vm = unsafe { jni::JavaVM::from_raw(vm).unwrap() };
+    livekit::webrtc::android::initialize_android(&vm);
+    
+    log::info!("LiveKit Android globals initialized");
+    
+    jni::sys::JNI_VERSION_1_6
+}
